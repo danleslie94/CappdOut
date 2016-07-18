0900101d34a9c66c8bd847bb1ea9dc8824d05139
Daniel Leslie <dan.leslie94@gmail.com>
2016-07-17 23:44:25 -0400

    First Commit
    First Commit to set up Cappd Out on Github

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BottleCap))]
+
+
+public class CanvasManager : MonoBehaviour {
+
+    public Canvas GameScreen;
+    public Canvas ResearchScreen;
+    public Canvas UpgradeScreen;
+
+    public Image background;
+
+    public Text cashText;
+    public Text cashTextInResearch;
+    public Text cashTextInUpgrades;
+
+    public Text BCSearchResult;
+    public Text BCSearchTime;
+    public GameObject BCResearchCost;
+    public GameObject BCValueUpCost;
+    public Button BCButton;
+    private BottleCap bottleCap;
+    private GameManager gameManager;
+    public bool isSearchingBC = false;
+
+    public float timeRemaining = 0.0f;
+
+    void Start()
+    {
+        ResearchScreen.enabled = false;
+        UpgradeScreen.enabled = false;
+        gameManager = GetComponent<GameManager>();
+        bottleCap = GetComponent<BottleCap>();
+
+    }
+    void Update()
+    {
+        cashText.fontSize = 20;
+        cashText.text = "$" + gameManager.GetPlayerCash();
+        cashTextInResearch.text = "$" + gameManager.GetPlayerCash();
+        cashTextInUpgrades.text = "$" + gameManager.GetPlayerCash();
+
+        BCResearchCost.GetComponent<Text>().text = "$" + bottleCap.bCResearchCost;
+        BCValueUpCost.GetComponent<Text>().text = "$" + bottleCap.bottleCapValueIncreaseCost;
+        
+
+        if (isSearchingBC)
+        {
+
+
+
+
+
+            displayBCSearchResult(0);
+            timeRemaining -= Time.deltaTime;
+            BCSearchTime.text = timeRemaining.ToString("#.0");
+            BCButton.interactable = false;
+            if (timeRemaining <= 0)
+            {
+                isSearchingBC = false;
+                float rando = Random.Range(gameManager.efficiencyCheckMin, gameManager.efficiencyCheckMax);
+                if (rando <= bottleCap.bCSearchEff)
+                {
+                    gameManager.IncreasePlayerCash(bottleCap.bottleCapValue);
+                    displayBCSearchResult(1);
+                }
+                else
+                {
+                    displayBCSearchResult(-1);
+                }
+            }
+     
+        }
+        else if(!isSearchingBC)
+        {
+            BCButton.interactable = true;
+            
+        }
+    }
+    public void displayBCSearchResult(int flag)
+    {
+        if (flag > 0)
+        {
+            BCSearchResult.text = "Success! Found a Bottlecap!";
+        }
+        else if (flag < 0)
+        {
+
+            BCSearchResult.text = "Nothing Found! Keep Searching!";
+        }
+        else
+        {
+            BCSearchResult.text = "";
+
+        }
+
+    }
+    public void goToResearch()
+    {
+        UpgradeScreen.enabled = false;
+        ResearchScreen.enabled = true;
+    }
+    public void incBCSearchEff()
+    {
+        if (gameManager.playerCash >= bottleCap.bCResearchCost)
+        {
+            gameManager.playerCash = gameManager.playerCash - bottleCap.bCResearchCost;
+            bottleCap.bCSearchEff += 2.5f;
+            bottleCap.bCResearchCost += 1.75f;
+        }
+    }
+    public void incBCValue()
+    {
+        if (gameManager.playerCash >= bottleCap.bottleCapValueIncreaseCost)
+        {
+            gameManager.playerCash = gameManager.playerCash - bottleCap.bottleCapValueIncreaseCost;
+            bottleCap.bottleCapValueIncreaseCost += 2.5f;
+            bottleCap.bottleCapValue += 0.05f;
+        }
+    }
+    public void researchToMain()
+    {
+        ResearchScreen.enabled = false;
+    }
+    public void goToUpgrades()
+    {
+        UpgradeScreen.enabled = true;
+    }
+    public void upgradesToMain()
+    {
+        UpgradeScreen.enabled = false;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class GameManager : MonoBehaviour 
+{
+	static GameManager _instance = null;
+    public static GameManager instance
+    {
+        get
+        {
+            return _instance;
+        }
+        set
+        {
+            _instance = value;
+        }
+    }
+    public float efficiencyCheckMin = 1.0f;
+	public float efficiencyCheckMax = 101.0f;
+	public float playerCash = 5.0f;
+
+    public float GetPlayerCash()
+    {
+        return playerCash;
+    }
+    public void IncreasePlayerCash(float cashIncrease)
+    {
+        playerCash = playerCash + cashIncrease;
+    }
+
+    void Start()
+    {
+        playerCash = 5.0f;
+    }
+
+	
+	
+
+
+	
+	
+	
+
+
+
+}
